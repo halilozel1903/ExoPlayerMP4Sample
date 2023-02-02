6d3be6d9e93d7ba6de802fd76084b7260edd3946
Halil Özel <halilozel1903@gmail.com>
2023-02-03 00:02:58 +0300

    ExoPlayer settings made for MP4.

--- app/src/main/java/com/halil/ozel/exoplayermp4sample/MainActivity.kt
+++ app/src/main/java/com/halil/ozel/exoplayermp4sample/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.halil.ozel.exoplayermp4sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.halil.ozel.exoplayermp4sample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+    private var playWhenReady = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        preparePlayer()
+    }
+
+    private fun preparePlayer() {
+        exoPlayer = ExoPlayer.Builder(this).build()
+        exoPlayer?.let { exo ->
+            exo.playWhenReady = true
+            binding.playerView.player = exo
+            exo.setMediaSource(setMediaType())
+            exo.seekTo(playbackPosition)
+            exo.playWhenReady = playWhenReady
+            exo.prepare()
+        }
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
+        }
+    }
+
+    private fun setMediaType(): MediaSource {
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem = MediaItem.fromUri(URL)
+        return ProgressiveMediaSource.Factory(defaultHttpDataSourceFactory)
+            .createMediaSource(mediaItem)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
+    companion object {
+        const val URL =
+            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
     }
 }
